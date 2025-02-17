ccfab257674d39b13d7e070c8f4179d867ec73b7
Hamza Sengul <138229016+Hamza-Sengul@users.noreply.github.com>
2025-02-17 12:59:14 +0300

    Update urls.py

--- qryoklama/qryoklama/urls.py
+++ qryoklama/qryoklama/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     # Ana sayfa: Akademisyen ve Öğrenci giriş butonlarını içeren basit bir şablon
     path('', TemplateView.as_view(template_name='home.html'), name='home'),
+    path('qr_attendance/<int:session_id>/', accounts_views.qr_attendance, name='qr_attendance'),
     path('accounts/', include('accounts.urls')),
 ]
